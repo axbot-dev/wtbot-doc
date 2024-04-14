710881dc15cd26a7f958329d683796e5e1afdc9c
axiangcoding <axiangcoding@gmail.com>
2024-04-15 00:43:57 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/axbot-dev/wtbot-doc/',
+            'https://github.com/axbot-dev/wtbot-doc/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
