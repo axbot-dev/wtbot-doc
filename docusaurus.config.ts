108462223656bb0a2e90d52f45f3bb1373821b37
axiangcoding <axiangcoding@gmail.com>
2024-04-15 00:47:59 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,13 +11,14 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wtbot-doc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'axbot-dev', // Usually your GitHub org/user name.
   projectName: 'wtbot-doc', // Usually your repo name.
-
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
